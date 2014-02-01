5a5cb777557287ad9a0f3694e920c7e03e4e5868
Marcelo de Moraes Serpa <fullofcaffeine@gmail.com>
2014-01-31 18:51:23 -0600

    Changes

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,8 @@ license          "All rights reserved"
 description      "Installs/Configures vagrant_main"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+
+ ['apache2','build-essential','apt','git','magento-toolbox','nodejs','mysql','oh-my-zsh','phing','openssl','php-box','python','composer','npm','php'].each do |d|
+   depends d
+ end
